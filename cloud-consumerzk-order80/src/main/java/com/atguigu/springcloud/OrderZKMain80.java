e99bf12a7672a3bc35d7eef2abf1fb1b7822f3ad
wangyidong111 <wangyidong_good@126.com>
2020-08-09 21:07:52 +0800

    springcloud2020

---
+++ cloud-consumerzk-order80/src/main/java/com/atguigu/springcloud/OrderZKMain80.java
@@ -0,0 +1,14 @@
+package com.atguigu.springcloud;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderZKMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZKMain80.class,args);
+    }
+}
---
+++ cloud-gateway-gateway9527/src/test/java/T1.java
@@ -0,0 +1,10 @@
+import java.time.ZonedDateTime;
+
+public class T1 {
+    public static void main(String[] args) {
+        ZonedDateTime zdt = ZonedDateTime.now();
+        System.out.println(zdt);
+        //2020-07-25T13:19:33.828+08:00[Asia/Shanghai]
+
+    }
+}
